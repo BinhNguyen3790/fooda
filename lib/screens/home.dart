f19024cb4fc7c1cb7f30ba10f7696a769350dff4
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2025-09-16 10:09:39 +0700

    create page home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -38,11 +38,374 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: Container(
-        child: ElevatedButton(
-          onPressed: (){
-            Navigator.of(context).popAndPushNamed('login');
-          }, 
-          child: Text("logout")
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Poular Foods",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "View All",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              height: 150, 
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          "assets/imgs/home/bg_item.png",
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 4,
+                                    right: 4,
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.favorite,
+                                        color: Colors.redAccent,
+                                      ),
+                                      onPressed: () {
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Text("Something something", style: TextStyle(fontWeight: FontWeight.bold),),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.timer,size: 15,),
+                                        Text("20min"),
+                                      ],
+                                    ),
+                                    Text("\$10.0",style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: (){}, 
+                                  icon: Icon(Icons.add),
+                                  color: Colors.white,
+                                  style: IconButton.styleFrom(
+                                    backgroundColor: Colors.redAccent,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ]
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              height: 150, 
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          "assets/imgs/home/bg_item.png",
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 4,
+                                    right: 4,
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.favorite,
+                                        color: Colors.redAccent,
+                                      ),
+                                      onPressed: () {
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Text("Something something", style: TextStyle(fontWeight: FontWeight.bold),),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.timer,size: 15,),
+                                        Text("20min"),
+                                      ],
+                                    ),
+                                    Text("\$10.0",style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: (){}, 
+                                  icon: Icon(Icons.add),
+                                  color: Colors.white,
+                                  style: IconButton.styleFrom(
+                                    backgroundColor: Colors.redAccent,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ]
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              height: 150, 
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          "assets/imgs/home/bg_item.png",
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 4,
+                                    right: 4,
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.favorite,
+                                        color: Colors.redAccent,
+                                      ),
+                                      onPressed: () {
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Text("Something something", style: TextStyle(fontWeight: FontWeight.bold),),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.timer,size: 15,),
+                                        Text("20min"),
+                                      ],
+                                    ),
+                                    Text("\$10.0",style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: (){}, 
+                                  icon: Icon(Icons.add),
+                                  color: Colors.white,
+                                  style: IconButton.styleFrom(
+                                    backgroundColor: Colors.redAccent,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ]
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              height: 150, 
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          "assets/imgs/home/bg_item.png",
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 4,
+                                    right: 4,
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.favorite,
+                                        color: Colors.redAccent,
+                                      ),
+                                      onPressed: () {
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Text("Something something", style: TextStyle(fontWeight: FontWeight.bold),),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.timer,size: 15,),
+                                        Text("20min"),
+                                      ],
+                                    ),
+                                    Text("\$10.0",style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),),
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: (){}, 
+                                  icon: Icon(Icons.add),
+                                  color: Colors.white,
+                                  style: IconButton.styleFrom(
+                                    backgroundColor: Colors.redAccent,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ]
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).popAndPushNamed('login');
+              },
+              child: Text("logout"),
+            ),
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.redAccent,
+        child: Icon(
+          Icons.search, 
+          color: Colors.white,
+          size: 40,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50)
+        ),
+        elevation: 0,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: (){}, 
+              icon: Icon(
+                Icons.home,
+                color: Colors.redAccent,                  
+                size: 30,
+              )
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.grey,                  
+                size: 30,
+              )
+            ),
+            SizedBox(width: 50), 
+            IconButton(
+              onPressed: (){}, 
+              icon: Icon(
+                Icons.shopping_basket_rounded,
+                color: Colors.grey,                  
+                size: 30,
+              )
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: Icon(
+                Icons.person_sharp,
+                color: Colors.grey,                  
+                size: 30,
+              )
+            ),
+          ],
         ),
       ),
     );
